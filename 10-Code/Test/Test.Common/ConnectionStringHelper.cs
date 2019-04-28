3834a28990bb917cbe4b99c826d69ed1b27bd03f
seventiny <seventiny@foxmail.com>
2019-04-28 21:27:12 +0800

    连接字符串去除

--- 10-Code/Test/Test.Common/ConnectionStringHelper.cs
+++ 10-Code/Test/Test.Common/ConnectionStringHelper.cs
@@ -2,7 +2,7 @@
 {
     public class ConnectionStringHelper
     {
-        public static string ConnectionString_Write = "123";//ConnectionStrings.Get("mysql39901")
+        public static string ConnectionString_Write = "";//ConnectionStrings.Get("mysql39901")
         public static string[] ConnectionStrings_Read = 
             new[] {
                 ConnectionString_Write,
